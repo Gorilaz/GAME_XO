9547a3179a4c2954efe71e897166814d25b9056a
Sergei <gavrserg89@mail.ru>
2019-05-17 13:53:52 +0300

    make a method chechNextMove

--- src/io/hexlet/view/AICoordinateGetter.java
+++ src/io/hexlet/view/AICoordinateGetter.java
@@ -12,51 +12,28 @@ import java.util.Random;
 
 public class AICoordinateGetter implements ICoordinateGetter {
     private static final Random RANDOM = new Random();
-
-    public Point getRandom(final Field field) {
-        Point randomPoint = new Point(RANDOM.nextInt(field.getSize()), RANDOM.nextInt(field.getSize()));
-        while (field.getFigure(randomPoint) != null) {
-            randomPoint = new Point(RANDOM.nextInt(field.getSize()), RANDOM.nextInt(field.getSize()));
-        }
-        return randomPoint;
-    }
+    Figure winner;
+    Point point;
+    WinnerController wc = new WinnerController();
 
     public Point getMoveCoordinate(final Field field) {
         Field fieldLocal = field;
-        WinnerController wc = new WinnerController();
-        Point point = new Point(0,0);
-        Figure winner;
-
         final Figure figure =  Figure.X;
         final Figure enemy = Figure.O;
 
+        chechNextMove(fieldLocal,figure);
+        chechNextMove(fieldLocal,enemy);
 
+        return new RandomCoordinateGetter().getMoveCoordinate(fieldLocal);
+    }
+
+    public Point chechNextMove(Field fieldLocal,Figure figure){
         for (int i=0; i<fieldLocal.getSize(); i++) {
             for(int j=0; j<fieldLocal.getSize(); j++) {
                 point = new Point(i,j);
                 if (fieldLocal.getFigure(point) == null) {
                     fieldLocal.setFigure(point, figure);
-                    winner = wc.getWinner(field);
-                    if (winner == Figure.X) {
-                        System.out.println("X - win's!");
-                        fieldLocal.setFigure(point,null);
-                        return point;
-                    }
-                    else {
-                        fieldLocal.setFigure(point,null);
-                    }
-                }
-
-            }
-        }
-
-
-        for (int i=0; i<fieldLocal.getSize(); i++) {
-            for(int j=0; j<fieldLocal.getSize(); j++) {
-                point = new Point(i,j);
-                if (fieldLocal.getFigure(point) == null) {
-                    fieldLocal.setFigure(point, enemy);
-                    winner = wc.getWinner(field);
+                    winner = wc.getWinner(fieldLocal);
                     if (winner == Figure.O) {
                         //System.out.println("X - win's!");
                         fieldLocal.setFigure(point,null);
@@ -66,13 +43,10 @@ public class AICoordinateGetter implements ICoordinateGetter {
                         fieldLocal.setFigure(point,null);
                     }
                 }
-
             }
         }
 
-        point = getRandom(field);
-        return point;
+        return null;
     }
-
 
 }
